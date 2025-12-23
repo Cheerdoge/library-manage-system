e9622efdc9e7bc75b725de42671c1a47d128ecdb
Cheerdoge <3218296001@qq.com>
2025-12-23 13:42:35 +0800

    add: 新增响应函数，修改request结构体的地址

--- web/responses.go
+++ web/responses.go
@@ -1,25 +1,43 @@
 package web
 
+import "github.com/gin-gonic/gin"
+
 type Response struct {
-	Code    int         `json:"code"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
 }
 
-// 错误响应函数
-func ErrorResponse(code int, message string) Response {
-	return Response{
+func NewResponse(c *gin.Context, code int, message string, data any) {
+	c.JSON(200, Response{
 		Code:    code,
 		Message: message,
-		Data:    nil,
-	}
+		Data:    data,
+	})
 }
 
-// 成功响应函数
-func SuccessResponse(message string, data interface{}) Response {
-	return Response{
-		Code:    200,
-		Message: "成功",
-		Data:    data,
-	}
+// SuccessResponse 成功响应
+func Ok(c *gin.Context, message string, data any) {
+	NewResponse(c, 200, message, data)
+}
+
+func OkWithData(c *gin.Context, data any) {
+	Ok(c, "操作成功", data)
+}
+
+func OkWithMessage(c *gin.Context, message string) {
+	Ok(c, message, gin.H{})
+}
+
+// ErrorResponse 失败响应
+func Fail(c *gin.Context, code int, message string) {
+	NewResponse(c, code, message, gin.H{})
+}
+
+func FailWithMessage(c *gin.Context, message string) {
+	Fail(c, 500, message)
+}
+
+func FailWithCode(c *gin.Context, code int) {
+	Fail(c, code, "操作失败")
 }
