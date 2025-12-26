0d9e7436e25548a27a11e1ac19bdd8bd3edef070
Cheerdoge <3218296001@qq.com>
2025-12-26 21:36:00 +0800

    feat: swagger, 但是没搞懂

--- web/requests.go
+++ web/requests.go
@@ -2,7 +2,7 @@ package web
 
 //===============Book==============
 
-//新增图书请求
+// 新增图书请求
 type AddBook struct {
 	Bookname string `json:"bookname"`
 	Author   string `json:"author"`
@@ -18,44 +18,44 @@ type AddBook struct {
 // 	BookId uint `json:"bookid"`
 // }
 
-//删除图书请求，分部分和全部
+// 删除图书请求，分部分和全部
 type DelBook struct {
 	Id  uint `json:"id"`
 	Num uint `json:"num"`
 }
 
-//更新图书信息，主要是数量
+// 更新图书信息，主要是数量
 type UpdateBook struct {
 	Num int `json:"num"`
 }
 
 //================User===============
 
-//用户注册请求
+// 用户注册请求
 type AddUser struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-//用户登录请求
+// 用户登录请求
 type LoginUser struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-//用户修改密码请求
+// 用户修改密码请求
 type ChangePassword struct {
 	Oldpassword string `json:"oldpassword"`
 	Newpassword string `json:"newpassword"`
 }
 
-//用户修改信息请求
+// 用户修改信息请求
 type ChangeUserInfo struct {
 	Username string `json:"username"`
 	Telenum  string `json:"telenum"`
 }
 
-//用户注销请求
+// 用户注销请求
 type DelUser struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -68,7 +68,7 @@ type DelUser struct {
 
 //================BorrowRecord===============
 
-//新增借书记录请求
+// 新增借书记录请求
 type BorrowBook struct {
 	BookNum int `json:"book_num"`
 }
